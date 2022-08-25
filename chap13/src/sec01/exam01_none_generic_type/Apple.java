cf14b780015864501e77e81bc63bd3afdc0e1649
SonYoungChae <thsdudco1234@naver.com>
2022-08-25 11:21:29 +0900

    java

---
+++ chap13/src/sec01/exam01_none_generic_type/Apple.java
@@ -0,0 +1,5 @@
+package sec01.exam01_none_generic_type;
+
+public class Apple {
+	
+}
---
+++ chap13/src/sec01/exam01_none_generic_type/Box.java
@@ -0,0 +1,14 @@
+package sec01.exam01_none_generic_type;
+
+public class Box {
+	private Object object;
+
+	public Object get() {
+		return object;
+	}
+
+	public void set(Object object) {
+		this.object = object;
+	}
+
+}
